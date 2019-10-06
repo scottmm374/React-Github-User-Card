4df562710dc8cde3b600b8262d434c22bfdea190
Michelle Scott <scottmm374@gmail.com>
2019-10-06 11:51:00 -0400

    Added fotter and credit for background image

--- github-card/src/App.js
+++ github-card/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MainHeader from './components/MainHeader'
+import MainFooter from './components/MainFooter'
 
 import './App.css';
 
@@ -7,6 +8,13 @@ function App() {
   return (
     <div className="App">
       <MainHeader />
+      <div>Main Content</div>
+      <section>
+      <footer>
+        <MainFooter />
+      </footer>
+      </section>
+      
     </div>
   );
 }
---
+++ github-card/src/components/MainFooter.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const NewFooter = styled.footer`
+font-family: 'Barriecito', cursive;
+font-size : 1rem;
+background: #b56969;
+padding: 1.5rem;
+margin-top: 800px;
+`;
+
+const MainFooter = () => {
+    return (
+        <NewFooter>
+            <span><p> Created by Michelle Scott</p><a target="blank" href="https://www.freepik.com/free-photos-vectors/background">Background vector created by macrovector - www.freepik.com</a></span>
+        </NewFooter>
+    )
+   
+}
+
+export default MainFooter;
