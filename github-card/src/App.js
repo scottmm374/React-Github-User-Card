15648b0e42d4371fd986076eee64a4ec3a72eee9
Michelle Scott <scottmm374@gmail.com>
2019-10-06 12:32:48 -0400

    Axios call user data, created UserCard

--- github-card/src/App.js
+++ github-card/src/App.js
@@ -1,22 +1,62 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios'
 import MainHeader from './components/MainHeader'
 import MainFooter from './components/MainFooter'
+import UserCard from './components/user/UserCard'
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <MainHeader />
-      <div>Main Content</div>
-      <section>
-      <footer>
-        <MainFooter />
-      </footer>
-      </section>
-      
-    </div>
-  );
+
+class App extends Component {
+  constuctor() {
+    this.state = {
+      userData: [] 
+    };
+  }
+
+  componentDidMount() {
+    axios 
+      .get('https://api.github.com/users/scottmm374')
+      .then(res => this.setState({ userData: res.data}))
+      .catch(err => console.log(err))
+  };
+
+
+  render() {
+    console.log("userData", this.state)
+    return (
+      <div className="App">
+        <MainHeader />
+        <div>
+          {userData.map(user => (
+             <UserCard 
+             key={user.id}
+             name={user.name}
+             profileName={user.login}
+             avatar={user.avatar_url}
+             github={user.url}
+             location={user.location}
+             bio={user.bio}
+             followers={user.followers}
+             created={user.created_at}
+             repos={user.public_repos}
+
+             
+             />
+          ))}
+         
+        </div>
+        <section>
+        <footer>
+          <MainFooter />
+        </footer>
+        </section>
+        
+      </div>
+    );
+
+  }
+ 
 }
 
 export default App;
---
+++ github-card/src/components/user/UserCard.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import {Card} from 'semantic-ui-react'
+
+const UserCard = props => {
+    console.log("userCard", props)
+
+    return(
+        <div>
+        
+            <Card>
+
+
+            </Card>
+
+        ))}
+        </div>
+       
+    )
+}
+
+export default UserCard;
